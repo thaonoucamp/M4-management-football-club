cc73febbb126ec8ab69da4e73510a36ab678bc09
thaonoucamp <dangxuanthaoht@gmail.com>
2021-07-25 19:08:41 +0700

    property of model

--- src/main/java/model/Coach.java
+++ src/main/java/model/Coach.java
@@ -1,4 +1,20 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Coach {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+    private String image;
+    private long salary;
+
 }
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,25 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Player {
+    @ManyToOne
+    private Coach coach;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+    private String image;
+    private String location;
+    private double height;
+    private double weight;
+    private double BMI;
+    private long salary;
+    private long bonus;
+    private String status;
+
 }
