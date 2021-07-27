11fb15437107ea96f39e8da2a293e6b5842f8af6
vuthanh199826 <vuthanh199825@gmail.com>
2021-07-27 16:04:43 +0700

    first commit

---
+++ src/main/java/service/player/PlayerService.java
@@ -0,0 +1,34 @@
+package service.player;
+
+import model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.IPlayerRepository;
+
+import java.util.Optional;
+
+@Service
+public class PlayerService implements IPlayerService {
+    @Autowired
+    IPlayerRepository playerRepository;
+
+    @Override
+    public Iterable<Player> findAll() {
+        return playerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Player> findById(Long id) {
+        return playerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Player player) {
+        playerRepository.save(player);
+    }
+
+    @Override
+    public void delete(Long id) {
+        playerRepository.deleteById(id);
+    }
+}
